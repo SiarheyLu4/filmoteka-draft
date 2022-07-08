b3fc0c51782a22f2f9a17c0eed8ad2fec2f40e24
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-07-08 17:47:11 +0300

    збільшення картки

--- src/js/renderFunctions.js
+++ src/js/renderFunctions.js
@@ -1,26 +1,28 @@
 import MovieApiService from './movieFetch';
 const movie = new MovieApiService();
 
-const containerCard = document.querySelector('.container-card')
+const containerCard = document.querySelector('.container-card');
 
 export async function renderMarkupPopular() {
-    const array = await movie.fetchPopular();
-    // console.log(array);
-    containerCard.insertAdjacentHTML('beforeend', makeMarkup(array));
+  const array = await movie.fetchPopular();
+  // console.log(array);
+  containerCard.insertAdjacentHTML('beforeend', makeMarkup(array));
 }
 
 export async function renderMarkupByQ(searchQuery) {
-    const array = await movie.fetchByQuery(searchQuery);
-    // console.log(array);
-    containerCard.innerHTML=await makeMarkup(array);
-    // containerCard.insertAdjacentHTML('beforeend', await makeMarkup(array));
+  const array = await movie.fetchByQuery(searchQuery);
+  // console.log(array);
+  containerCard.innerHTML = await makeMarkup(array);
+  // containerCard.insertAdjacentHTML('beforeend', await makeMarkup(array));
 }
 
-function makeMarkup(array){
-    return array.map(({ poster_path, id, original_title, release_date, genre_ids }) => {
-        // const newReleaseDate = release_date.split('-')[0];
-        return `
+function makeMarkup(array) {
+  return array
+    .map(({ poster_path, id, original_title, release_date, genre_ids }) => {
+      // const newReleaseDate = release_date.split('-')[0];
+      return `
                   <div class="container-card_single-card" data-id="${id}" >
+                  <a href="" src="">
                     <div class="poster">
                         <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${original_title}">
                     </div>
@@ -30,6 +32,9 @@ function makeMarkup(array){
                           ${genre_ids} | ${release_date}
                       </p>
                     </div>
+                  </a>
                   </div> 
-        `}).join('');
+        `;
+    })
+    .join('');
 }
